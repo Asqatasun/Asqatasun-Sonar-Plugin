0cb6521b9e6319e639db45b44d55bc813e7db20b
Jérôme Kowalczyk <jk@tanaguru.com>
2015-02-03 12:15:27 +0100

    add unit test context

---
+++ src/test/java/org/sonar/plugins/tanaguru/checks/TestHelper.java
@@ -0,0 +1,61 @@
+/*
+ * SonarQube Tanaguru Plugin
+ * Copyright (C) 2015 Tanaguru.org
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.tanaguru.checks;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import org.sonar.plugins.web.analyzers.ComplexityVisitor;
+import org.sonar.plugins.web.analyzers.PageCountLines;
+import org.sonar.plugins.web.lex.PageLexer;
+import org.sonar.plugins.web.node.Node;
+import org.sonar.plugins.web.visitor.DefaultNodeVisitor;
+import org.sonar.plugins.web.visitor.HtmlAstScanner;
+import org.sonar.plugins.web.visitor.WebSourceCode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+public class TestHelper {
+
+  private TestHelper() {
+  }
+
+  public static WebSourceCode scan(File file, DefaultNodeVisitor visitor) {
+    FileReader fileReader;
+    try {
+      fileReader = new FileReader(file);
+    } catch (FileNotFoundException e) {
+      throw Throwables.propagate(e);
+    }
+    PageLexer lexer = new PageLexer();
+    List<Node> nodes = lexer.parse(fileReader);
+    WebSourceCode result = new WebSourceCode(file, new org.sonar.api.resources.File("test"));
+
+    HtmlAstScanner walker = new HtmlAstScanner(ImmutableList.of(new PageCountLines(), new ComplexityVisitor()));
+    walker.addVisitor(visitor);
+    walker.scan(nodes, result, Charsets.UTF_8);
+
+    return result;
+  }
+
+}
---
+++ src/test/java/org/sonar/plugins/tanaguru/checks/rgaa3/Rgaa308031CheckTest.java
@@ -0,0 +1,115 @@
+/*
+ * SonarQube Tanaguru Plugin
+ * Copyright (C) 2015 Tanaguru.org
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.tanaguru.checks.rgaa3;
+
+import org.junit.Ignore;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.sonar.plugins.tanaguru.checks.CheckMessagesVerifierRule;
+import org.sonar.plugins.tanaguru.checks.TestHelper;
+import org.sonar.plugins.web.visitor.WebSourceCode;
+import java.io.File;
+
+public class Rgaa308031CheckTest {
+
+  @Rule
+  public CheckMessagesVerifierRule checkMessagesVerifier = new CheckMessagesVerifierRule();
+
+  @Test
+  public void doctype_before_html() throws Exception {
+    WebSourceCode sourceCode = TestHelper
+            .scan(new File(
+                            "src/test/resources/checks/rgaa3/Rgaa308031Check/DoctypeBeforeHtml.html"),
+                    new Rgaa308031Check());
+
+    checkMessagesVerifier.verify(sourceCode.getViolations()).noMore();
+  }
+
+  @Test
+  public void full_doctype_before_html() throws Exception {
+    WebSourceCode sourceCode = TestHelper
+            .scan(new File(
+                            "src/test/resources/checks/rgaa3/Rgaa308031Check/FullDoctypeBeforeHtml.html"),
+                    new Rgaa308031Check());
+
+    checkMessagesVerifier.verify(sourceCode.getViolations()).noMore();
+  }
+
+  @Test
+  public void no_doctype_before_foo() throws Exception {
+    WebSourceCode sourceCode = TestHelper
+            .scan(new File(
+                            "src/test/resources/checks/rgaa3/Rgaa308031Check/NoDoctypeBeforeFoo.html"),
+                    new Rgaa308031Check());
+
+    checkMessagesVerifier.verify(sourceCode.getViolations()).noMore();
+  }
+
+  @Test
+  public void no_doctype_before_html() throws Exception {
+    WebSourceCode sourceCode = TestHelper
+            .scan(new File(
+                            "src/test/resources/checks/rgaa3/Rgaa308031Check/NoDoctypeBeforeHtml.html"),
+                    new Rgaa308031Check());
+
+    checkMessagesVerifier.verify(sourceCode.getViolations()).noMore();
+  }
+
+  @Ignore
+  public void multiple_html_tags() throws Exception {
+    WebSourceCode sourceCode = TestHelper
+            .scan(new File(
+                            "src/test/resources/checks/rgaa3/Rgaa308031Check/MultipleHtmlTags.html"),
+                    new Rgaa308031Check());
+
+    checkMessagesVerifier.verify(sourceCode.getViolations()).next()
+            .atLine(1)
+            .withMessage(Rgaa308031Check.WRONG_DOCTYPE_POSITION)
+            .noMore();
+  }
+
+  @Test
+  public void doctype_after_html() throws Exception {
+    WebSourceCode sourceCode = TestHelper
+            .scan(new File(
+                            "src/test/resources/checks/rgaa3/Rgaa308031Check/DoctypeAfterHtml.html"),
+                    new Rgaa308031Check());
+
+    checkMessagesVerifier.verify(sourceCode.getViolations()).next()
+            .atLine(1)
+            .withMessage(Rgaa308031Check.WRONG_DOCTYPE_POSITION)
+            .noMore();
+  }
+
+  @Test
+  public void no_doctype_after_html() throws Exception {
+    WebSourceCode sourceCode = TestHelper
+            .scan(new File(
+                            "src/test/resources/checks/rgaa3/Rgaa308031Check/DoctypeAfterHtml.html"),
+                    new Rgaa308031Check());
+
+    checkMessagesVerifier.verify(sourceCode.getViolations()).next()
+            .atLine(1)
+            .withMessage(Rgaa308031Check.WRONG_DOCTYPE_POSITION)
+            .noMore();
+  }
+
+}
